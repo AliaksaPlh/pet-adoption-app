bb236657c31c4ff67f4cf5e6235514c4453e1585
Aliaksa <sashvero@gmail.com>
2025-10-15 16:37:48 +0300

    feat: add desxriptions text ru/en

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -1,15 +1,18 @@
 import { getRequestConfig } from 'next-intl/server';
 import { hasLocale } from 'next-intl';
 import { routing } from './routing';
+import enMessages from '@/../messages/en.json';
+import ruMessages from '@/../messages/ru.json';
 
 export default getRequestConfig(async ({ requestLocale }) => {
   const requested = await requestLocale;
   const locale = hasLocale(routing.locales, requested)
     ? requested
     : routing.defaultLocale;
+  const messages = locale === 'ru' ? ruMessages : enMessages;
 
   return {
     locale,
-    messages: (await import(`../../messages/${locale}.json`)).default,
+    messages,
   };
 });
