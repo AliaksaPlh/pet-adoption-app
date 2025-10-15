999298c4b7b74e0b080496e6fdc18030337e1ab7
Aliaksa <sashvero@gmail.com>
2025-10-15 17:03:10 +0300

    feat: add logo

---
+++ src/components/ui/logo/logo.tsx
@@ -0,0 +1,21 @@
+﻿import styles from './logo.module.scss';
+import Link from 'next/link';
+import { ROUTES } from '@/constants/routes';
+import { useTranslations } from 'next-intl';
+import Image from 'next/image';
+
+export default function Logo() {
+  const t = useTranslations('Header');
+  return (
+    <Link href={ROUTES.HOME} className={styles.logo}>
+      <Image
+        src="/pets.png"
+        alt={t('logo')}
+        width={35}
+        height={35}
+        className={styles.logoIcon}
+      />
+      {t('logo')}
+    </Link>
+  );
+}
