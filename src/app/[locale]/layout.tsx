8c8f4e9ef5d31252f8cf3696a41a2e43ff9d64df
Aliaksa <sashvero@gmail.com>
2025-10-15 16:41:58 +0300

    feat: add layout files

---
+++ src/app/[locale]/layout.tsx
@@ -0,0 +1,42 @@
+import type { Metadata } from 'next';
+// import '@/styles/index.scss';
+import { notFound } from 'next/navigation';
+import { hasLocale, NextIntlClientProvider } from 'next-intl';
+import { routing } from '@/i18n/routing';
+import Header from '@/components/layout/header/header';
+import { getMessages } from 'next-intl/server';
+
+export const metadata: Metadata = {
+  title: 'Pet Shelter - Find Your New Best Friend',
+  description:
+    'Find your new best friend whith the help of our pet helpers comunity. There are so many pets waiting for a loving home.',
+  icons: {
+    icon: '/pets.svg',
+  },
+};
+
+export default async function RootLayout({
+  children,
+  params,
+}: Readonly<{
+  children: React.ReactNode;
+  params: Promise<{ locale: string }>;
+}>) {
+  const { locale } = await params;
+  if (!hasLocale(routing.locales, locale)) {
+    notFound();
+  }
+
+  const messages = await getMessages();
+
+  return (
+    <html lang={locale}>
+      <body>
+        <NextIntlClientProvider locale={locale} messages={messages}>
+          <Header />
+          {children}
+        </NextIntlClientProvider>
+      </body>
+    </html>
+  );
+}
