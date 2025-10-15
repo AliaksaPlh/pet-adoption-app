30d031553538857a4264a9255b840ef433ee44b9
Aliaksa <sashvero@gmail.com>
2025-10-15 16:46:36 +0300

    refactor: set page

---
+++ src/app/[locale]/page.tsx
@@ -0,0 +1,14 @@
+'use client';
+
+import Page from '@/components/layout/page/page';
+import HomeContent from '@/components/ui/home-content/home-content';
+
+export default function Home() {
+  return (
+    <Page>
+      <div>
+        <HomeContent />
+      </div>
+    </Page>
+  );
+}
---
+++ src/components/layout/page/page.tsx
@@ -0,0 +1,16 @@
+﻿'use client';
+import styles from './page.module.scss';
+import clsx from 'clsx';
+
+type PageProps = {
+  children: React.ReactNode;
+  centered?: boolean;
+};
+
+export default function Page({ children, centered }: PageProps) {
+  return (
+    <main className={clsx(styles.main, centered && styles.centered)}>
+      <div className={'container'}>{children}</div>
+    </main>
+  );
+}
